741f9bf3c7014b04a9ffbc2161b8dfaed5740f5e
John Fuller <fuller.john84@gmail.com>
2023-11-06 18:24:35 -0500

    Still working on assignment.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Unit2.froyo
+
+Submitted incomplete for now, but will resubmit once i have a better understanding and can complete the task.
---
+++ main.js
@@ -0,0 +1,27 @@
+// prompt visitor for a comma-separated list of flavors.
+let userObject = {};
+let userInput = prompt(
+    "Please enter your flavors in a comma-separated list:"
+);
+userObject.data = userInput;
+
+// split flavors into array.
+let array = userInput.split(", ");
+
+// create a function that iterates through the user input, and tracks how many of each flavor.
+function getFlavors(){
+
+};
+
+// create table.
+
+
+
+// display the flavors and amounts of each
+console.log();
+
+
+
+
+
+// vanilla,vanilla,vanilla,strawberry,coffee,coffee
